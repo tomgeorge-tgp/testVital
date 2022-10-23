f4840e131c76e7aaa7427a86f4e2e1defa21df41
tomgeorge_tgp <tomgeorgetgp@gmail.com>
2022-10-23 12:52:34 +0530

    the pop up part functionality set css still small issues

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -9,10 +9,15 @@ import Lastreadings from '../components/Lastreadings';
 import bp_icon from '../components/assets/bp.png';
 import axios from 'axios';
 import api from '../api';
+import "./style/dashBoard.css"
+
+import PopUp from '../components/PopUp';
+
 
 function Dashboard(){
  
   const location=useLocation();
+  const [popUp,setPopUp]=useState("");
 
   // const [userData,setUserData]=useState([]);
   
@@ -53,91 +58,47 @@ function Dashboard(){
               <div className='readings'>
                    <div className='profile-card'><Profile data={location.state.name}/></div> 
                   <div className='reading-cards'>
-                   <Card name = "Blood Pressure" img  = {bp_icon} onClick={(e)=>{
-                console.log("click");}}
-                   ></Card>
+                {popUp && <div className="pop__up"><PopUp setPopUp={setPopUp}/></div>}
+                  <div onClick={(e)=>{
+                console.log("click");
+                 setPopUp("Blood Pressure")}}>
+                 <Card name = "Blood Pressure" img  = {bp_icon}></Card>
+                 </div>
+
+
                    <div onClick={(e)=>{
-                console.log("click");}} ><Card name = "Heart Rate & SP02" img  = {bp_icon}></Card></div>
+                console.log("click")
+                setPopUp("Heart Rate")}}>
+                <Card name = "Heart Rate & SP02" img  = {bp_icon}></Card>
+                </div>
+
+                <div onClick={(e)=>{
+                console.log("click");
+                 setPopUp("Body Temperature")}}>
                    <Card name = "Body Temperature" img  = {bp_icon}></Card>
+                   </div>
+                   <div onClick={(e)=>{
+                console.log("click");
+                 setPopUp("Blood Glucose")}}>
                    <Card name = "Blood Glucose" img  = {bp_icon}></Card>
+                   </div>
+                   <div onClick={(e)=>{
+                console.log("click");
+                 setPopUp("Digital Stethoscope")}}>
                    <Card name = "Digital Stethoscope" img  = {bp_icon}></Card>
+                   </div>
+                   <div onClick={(e)=>{
+                console.log("click");
+                 setPopUp("Ecg")}}>
                    <Card name = "ECG" img  = {bp_icon}></Card>
+                   </div>
                   </div>
               </div>
               <Lastreadings name1="Blood Pressure" name2="Heart Rate" name3="SpO2" name4="Body Temperature" name5="Blood Glucose" values = {[99,140,96,98,97,86]} ></Lastreadings>  
             </div>
           </div>
         </div>
-        <style jsx>
-          {`
-           *{
-            margin:0;
-            padding:0;
-           }
-            .dashboard-container {
-              width: 100%;
-              display: flex;
-              min-height: 2000px;
-              background-color: #FFFFFF;
-            }
-            .dashboard-container1 {
-              top: 0px;
-              flex: 0 0 auto;
-              left: 0px;
-              right: 0px;
-              width: 64rem;
-              bottom: 0px;
-              height: 600px;
-              margin: auto;
-              display: flex;
-              position: absolute;
-              align-items: flex-start;
-              flex-direction : column;
-              background-color: #FFFFFF;
-              
-            }
-            .mid-section{
-              background-color:#F5F5F5;
-              width :64rem;
-              height:100%;
-              display:flex;
-              flex-direction:column;
-             
-            }
-
-            .readings{
-              max-width:100%;
-              min-height:23rem;
-              display:flex;
-              justify-content:space-between;
-              align-items:center;
-              background-color:#F5F5F5;
-              margin-top:1rem;
-              margin-left:1.5rem;
-            }
-
-            
-
-           
-            .profile-card{
-              {/* height:22rem;
-              width:16rem;
-              padding:0;
-              background-color:#FFFF;
-              border-radius:20px */}
-            }
-
-            .reading-cards{
-              
-              width:46.5rem;
-              background-color:#F5F5F5;
-              display:flex;
-              align-items:center;
-              flex-wrap:wrap;
-              margin:0;
-            }
-          `}
-        </style>
+      
       </>
     )
   }
